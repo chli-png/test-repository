804f081dc5b4c7d69866069c4b1f3c816cad9a8a
chli-png <chlidev3@gmail.com>
2025-08-13 14:59:00 +0200

    Update HelloWorld.java
    This was added within github.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("This is a small change.");
+		System.out.println("This was added from github.");
 	}
 }
